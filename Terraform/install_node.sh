1e50100318909bb4fba354aa24f1fc8ce19866e9
Hai Le Ngoc (CN-PTCN TNCN) <hailn2@msb.com.vn>
2022-01-15 20:11:53 +0700

    Update user data of ec2 instance and provide diagram

--- Terraform/install_node.sh
+++ Terraform/install_node.sh
@@ -5,7 +5,7 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 nvm install node
 cd ~
 sudo yum install git -y
-git clone https://github.com/lehai2909/terrafrom-web.git
-cd terrafrom-web
+git clone https://github.com/lehai2909/Simple-web-AWS-Terraform.git
+cd Simple-web-AWS-Terraform/Web-nodejs
 npm install
 npm start
